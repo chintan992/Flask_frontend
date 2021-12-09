8a619c51797cddf6d346c57ea541dbf41c5c2471
chintan992 <chintanr21@gmail.com>
2021-12-09 22:45:01 +0530

    server

--- main.py
+++ main.py
@@ -3,7 +3,13 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-   return 'Hello World’
+   return '1234'
+
+@app.route('/hello')
+def hello_world1():
+   return 'hello world'
+app.add_url_rule('/', 'hello', hello_world1)
+
 
 if __name__ == '__main__':
-   app.run()
+   app.run(debug = True)
